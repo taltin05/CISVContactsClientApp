dbdfee198a0b5e744d1749aa82fd8ea1c5c627d6
Tunc Altinbas <TAltinbas@ows.test.lab>
2018-07-31 17:13:48 -0400

    Added home component, contacts-grid component (enabled paginator), contact.service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { MatTableModule } from '@angular/material';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactService } from './contact.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ContactsGridComponent } from './contacts-grid/contacts-grid.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent } 
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule
+    
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ app/contacts-grid/contacts-grid.component.ts
@@ -0,0 +1,29 @@
+import { Component,  OnInit, Input, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { ContactService } from '../contact.service';
+
+
+@Component({
+  selector: 'app-contacts-grid',
+  templateUrl: './contacts-grid.component.html',
+  styleUrls: ['./contacts-grid.component.css']
+})
+export class ContactsGridComponent implements OnInit {
+  
+  contactsData = new MatTableDataSource<any>([]);
+  displayedColumns: string[] = ['lastName', 'firstName', 'gender', 'addr1', 'city', 'state', 'zip', 'homePhone', 'mobilePhone', 'email'];
+  
+  constructor(private contactService: ContactService) {  
+    contactService.get().subscribe(
+      (data:any) => {this.contactsData.data = data}
+    );
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  
+  ngOnInit() {    
+    this.contactsData.paginator = this.paginator;
+  }
+
+  
+}
