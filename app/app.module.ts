e130fca43cee86757847313f8ef5a2e87505e6c8
Tunc Altinbas <TAltinbas@ows.test.lab>
2019-10-04 14:19:36 -0400

    Adding tabs on Contact Details page

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,6 +10,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material';
+import { MatTabsModule} from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -17,6 +18,8 @@ import { ContactService } from './contact.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ContactsGridComponent } from './contacts-grid/contacts-grid.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
+
+
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -47,7 +50,8 @@ const appRoutes: Routes = [
     MatSelectModule,
     MatCheckboxModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatTabsModule
   ],
   providers: [
     ContactService
